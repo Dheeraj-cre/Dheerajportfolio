dfe52dd228be22cb720bcfd5db2084be1e44adf5
Dheeraj-cre <Dheerajn2311@gmail.com>
2025-12-25 14:27:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Login from "./components/login";
+import Signup from "./components/Signup";
+
+function App() {
+  return (
+    <>
+      {/* Change component to test */}
+      {/* <Login /> */}
+      <Signup />
+    </>
+  );
+}
+
+export default App;
